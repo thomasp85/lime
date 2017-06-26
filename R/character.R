bad6b65398bd633db73b9e972f7e2319d282c900
pommedeterresautee <pommedeterresautee@msn.com>
2017-06-26 20:34:45 +0200

    cleaning

--- R/character.R
+++ R/character.R
@@ -1,41 +1,9 @@
-##' @importFrom stringdist seq_dist
-##' @importFrom dplyr bind_rows
-##' @export
-# lime.character <- function(x, model, preprocess = NULL, split_at = '\\W+', bow = TRUE, kernel_width = 25, ...) {
-#   kernel <- exp_kernel(kernel_width)
-#   if (is.null(preprocess)) preprocess <- function(x, ...) x
-#   function(cases, labels, n_labels = NULL, n_features, n_permutations = 5000, dist_fun = 'cosine', feature_select = 'auto') {
-#     case_perm <- permute_cases(cases, n_permutations, split_at, bow)
-#     case_perm_mat <- case_perm$tabular
-#     case_perm <- case_perm$permutations
-#     case_res <- predict(model, preprocess(case_perm, ...), type = 'prob')
-#     case_ind <- split(seq_len(nrow(case_perm)), rep(seq_len(nrow(cases)), each = n_permutations))
-#     res <- lapply(seq_along(case_ind), function(ind) {
-#       i <- case_ind[[ind]]
-#       dist <- seq_dist(list(case_perm_mat[[ind]][1, ]),
-#                        split(case_perm_mat[[ind]], seq_along(i)),
-#                        method = dist_fun)
-#       res <- model_permutations(as.matrix(case_perm_mat[[ind]]), case_res[i, ], kernel(dist), labels, n_labels, n_features, feature_select)
-#       res$feature_value <- unlist(case_perm[i[1], res$feature])
-#       res$feature_desc <- res$feature
-#       guess <- which.max(abs(case_res[i[1], ]))
-#       res$case <- rownames(cases)[ind]
-#       res$label_prob <- unname(as.matrix(case_res[i[1], ]))[match(res$label, names(case_res))]
-#       res$data <- list(as.list(case_perm[i[1], ]))
-#       res$prediction <- list(as.list(case_res[i[1], ]))
-#       res
-#     })
-#     res <- bind_rows(res)
-#     res[, c('case', 'label', 'label_prob', 'model_r2', 'model_intercept', 'feature', 'feature_value', 'feature_weight', 'feature_desc', 'data', 'prediction')]
-#   }
-# }
-
 ## TODO Add warning about bow when set to T, must manage new words
 ## Check parameter labels != NULL OR n_labels != NULL
 #' @importFrom stringdist seq_dist
 #' @importFrom magrittr %>%
 #' @export
-lime.character <- function(x, model, preprocess = NULL, split_by = "\\W+", bow = FALSE, kernel_width = 25,
+lime.character <- function(x, model, preprocess, split_by = "\\W+", bow = FALSE, kernel_width = 25,
                            n_permutations = 5000, number_features_explain = 5, feature_selection_method = "auto",
                            labels = NULL, n_labels = NULL,  dist_fun = "cosine", prediction = do.predict, ...) {
   permutation.cases <- permute_cases.character(x, n_permutations, split_by, bow, dist_fun)
--- R/permute_cases.R
+++ R/permute_cases.R
@@ -21,42 +21,6 @@ permute_cases.data.frame <- function(cases, n_permutations, feature_distribution
   perm[seq.int(1, by = n_permutations, length.out = nrow(cases)), ] <- cases
   perm
 }
-#' #' @importFrom stringi stri_split stri_join_list
-#' #' @importFrom Matrix Matrix
-#' permute_cases.character <- function(cases, n_permutations, split_by, bow) {
-#'   tokens <- stri_split(cases, regex = split_by)
-#'   doc_size <- lengths(tokens)
-#'   if (bow) {
-#'     tokens_lower <- split(tolower(unlist(tokens)), rep(seq_along(tokens), doc_size))
-#'     tokens_lookup <- lapply(tokens_lower, unique)
-#'     doc_size <- lengths(lapply(tokens_lookup, unique))
-#'   } else {
-#'     tokens_lookup <- tokens
-#'   }
-#'   perm <- lapply(seq_along(tokens_lookup), function(i) {
-#'     remove_n <- sample(doc_size[i], n_permutations, replace = FALSE)
-#'     remove <- lapply(remove_n, sample, x = doc_size[i] - 1)
-#'     permutations <- lapply(remove, function(ii) tokens_lookup[[i]][-ii])
-#'     if (bow) {
-#'       permutations <- lapply(permutations, function(words) tokens[[i]][!tokens_lower[[i]] %in% words])
-#'       var_names <- tokens_lookup[[i]]
-#'     } else {
-#'       var_names <- paste0(seq_along(tokens_lookup[[i]]), '_', tokens_lookup[[i]])
-#'     }
-#'     complete <- rep(1, length(tokens_lookup[[i]]))
-#'     remove <- do.call(rbind, lapply(remove, function(ii) {complete[ii] <- 0; complete}))
-#'     remove[1, ] <- 1
-#'     remove <- Matrix(remove, sparse = TRUE)
-#'     colnames(remove) <- tokens_lookup[[i]]
-#'     list(permutations, remove)
-#'   })
-#'   removes <- lapply(perm, `[[`, 2)
-#'   perm <- lapply(perm, `[[`, 1)
-#'   perm <- stri_join_list(unlist(perm, recursive = FALSE), sep = ' ')
-#'   perm[seq.int(1, by = n_permutations, length.out = length(cases))] <- cases
-#'   list(permutations = perm, tabular = removes)
-#' }
-
 
 #' @importFrom Matrix Matrix
 #' @importFrom purrr map map2 flatten_chr set_names flatten flatten_int set_colnames map_chr flatten_dbl
