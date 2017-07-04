fc686793de2d6f20d0d0243611681cf25910f6c9
pommedeterresautee <pommedeterresautee@msn.com>
2017-07-04 14:56:46 +0200

    dynamic loading

--- src/lime_init.c
+++ src/lime_init.c
@@ -22,5 +22,5 @@ static const R_CallMethodDef CallEntries[] = {
 void R_init_lime(DllInfo *dll)
 {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-    R_useDynamicSymbols(dll, FALSE);
+    R_useDynamicSymbols(dll, TRUE);
 }
