8d4270d51e65689627659cdcb19f58935d96000c
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-12-01 15:57:20 +0100

    Mark constant numeric features as such and treat them as constants (#57)

--- R/permute_cases.R
+++ R/permute_cases.R
@@ -5,7 +5,9 @@ permute_cases <- function(cases, n_permutations, ...) {
 permute_cases.data.frame <- function(cases, n_permutations, feature_distribution, bin_continuous, bin_cuts) {
   nrows <- nrow(cases) * n_permutations
   perm <- as.data.frame(lapply(seq_along(cases), function(i) {
-    perms <- if (is.numeric(cases[[i]]) && bin_continuous) {
+    perms <- if (is.numeric(cases[[i]]) && is.na(feature_distribution[[i]])) {
+      rep(cases[[i]], each = n_permutations)
+    } else if (is.numeric(cases[[i]]) && bin_continuous) {
       bin <- sample(seq_along(feature_distribution[[i]]), nrows, TRUE, as.numeric(feature_distribution[[i]]))
       diff(bin_cuts[[i]])[bin] * runif(nrows) + bin_cuts[[i]][bin]
     } else if (is.numeric(cases[[i]])) {
