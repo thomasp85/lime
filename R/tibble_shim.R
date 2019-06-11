5316eb0418db6347efef94199d8521c147bee83f
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-06-11 22:25:33 +0200

    Fix tibble shim

--- R/tibble_shim.R
+++ R/tibble_shim.R
@@ -1,5 +1,5 @@
 # Avoid depending on tibble but provide nice printing for people who have it
 as_tibble <- function(x) {
-  class(x) <- c('tbl_df', class(x))
+  class(x) <- c('tbl_df', 'tbl', class(x))
   x
 }
