adbff174bd30fbd95f501d7e99ee59684ddfb778
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-06-12 14:08:47 +0200

    Update docs

--- R/plot.R
+++ R/plot.R
@@ -93,7 +93,7 @@ plot_features <- function(explanation, ncol = 2, cases = NULL) {
 #' # Create some explanations
 #' library(MASS)
 #' iris_test <- iris[c(1, 51, 101), 1:4]
-#' iris_train <- iris[-1, 1:4]
+#' iris_train <- iris[-c(1, 51, 101), 1:4]
 #' iris_lab <- iris[[5]][-1]
 #' model <- lda(iris_train, iris_lab)
 #' explanation <- lime(iris_train, model)
